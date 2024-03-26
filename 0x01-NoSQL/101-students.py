6240b5600f7537407c3cae738f4071ce391b9194
Mcrymbo <croco2023@gmail.com>
2024-03-26 11:58:31 -0700

    sorted list

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+returns all students sorted by average score
+"""
+
+
+def top_students(mongo_collection):
+    """ function that returns all students sorted by av score """
+    sorted_list = mongo_collection.aggregate([
+         {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+    return sorted_list
